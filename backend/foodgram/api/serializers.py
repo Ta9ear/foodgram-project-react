2ffccef2abd5447199e88cb343fced87a88eac05
Ta9ear <Ta.9ear@gmail.com>
2023-01-11 16:41:43 +0300

    v1.0.1 validation

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import exceptions, serializers, validators
+from rest_framework import exceptions, serializers
 
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             ShoppingCart, Tag)
